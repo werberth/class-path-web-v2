55002b3299f1c70f97faae6dcd8c5c21e0838c03
Werberth <werberthvinicius@gmail.com>
2019-12-16 05:11:22 -0300

    feat: add home view

--- class_path_web/urls.py
+++ class_path_web/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('class_path_web.core.urls', 'core'), namespace="core"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
