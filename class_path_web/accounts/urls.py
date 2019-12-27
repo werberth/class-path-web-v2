9b9a3c79aa099f63b4890d9756abb3a3e2ab3fb2
Werberth <werberthvinicius@gmail.com>
2019-12-27 04:42:42 -0300

    feat: add login template

--- class_path_web/accounts/urls.py
+++ class_path_web/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 
 urlpatterns = [
     path('sign-up/', views.signup, name='sign-up'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
 ]
