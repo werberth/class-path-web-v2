e043038909676dc8936e4760fb950c7377893f49
Werberth <werberthvinicius@gmail.com>
2020-01-03 07:19:54 -0300

    feat: add delete and update activity views

--- class_path_web/content/urls.py
+++ class_path_web/content/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('create-content/', views.create_content, name='create-content'),
     path('create-activity/', views.create_activity, name='create-activity'),
     path('update-content/<int:pk>/', views.update_content, name='update-content'),
+    path('update-activity/<int:pk>/', views.update_activity, name='update-activity'),
     path('delete-content/<int:pk>/', views.delete_content, name='delete-content'),
+    path('delete-activity/<int:pk>/', views.delete_activity, name='delete-activity'),
     path('list-content/', views.list_content, name='list-content'),
     path('list_activity/', views.list_activity, name='list-activity')
 ]
--- class_path_web/content/views.py
+++ class_path_web/content/views.py
@@ -27,11 +27,11 @@ class CreateContentView(base_core_views.BaseFormView, generic.CreateView):
 class CreateActivityView(base_core_views.BaseFormView, generic.CreateView):
     form_class = forms.ActivityForm
     template_title = 'Criar Atividade'
-    success_url = r('core:dashboard')
+    success_url = r('content:list-activity')
     template_name = 'content/activity/activity_form.html'
 
     def get_form_kwargs(self):
-        kwargs = super(CreateActivityView, self).get_form_kwargs()
+        kwargs = super().get_form_kwargs()
         kwargs['request'] = self.request
         return kwargs
 
@@ -48,6 +48,23 @@ class UpdateContentView(
     template_name = 'content/content_form.html'
     context_object_name = 'content'
 
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('accounts.is_teacher'), name='dispatch')
+class UpdateActivityView(
+    base_views.ActivityBaseQueryset,
+    base_core_views.BaseFormView,
+    generic.UpdateView):
+    form_class = forms.ActivityForm
+    success_url = r('content:list-activity')
+    template_title = 'Editar Atividade'
+    template_name = 'content/activity/activity_form.html'
+    context_object_name = 'activity'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
 
 @method_decorator(login_required, name='dispatch')
 @method_decorator(permission_required('accounts.is_teacher'), name='dispatch')
@@ -72,9 +89,20 @@ class DeleteLocationView(
     success_url = r('content:list-content')
 
 
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('accounts.is_teacher'), name='dispatch')
+class DeleteActivityView(
+    base_views.ActivityBaseQueryset,
+    base_core_views.BaseDelete):
+
+    success_url = r('content:list-activity')
+
+
 create_content = CreateContentView.as_view()
 create_activity = CreateActivityView.as_view()
 update_content = UpdateContentView.as_view()
+update_activity = UpdateActivityView.as_view()
 list_content = ListContentView.as_view()
 list_activity = ListActivityView.as_view()
 delete_content = DeleteLocationView.as_view()
+delete_activity = DeleteActivityView.as_view()
