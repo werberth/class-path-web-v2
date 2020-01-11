3de508af7eee7275c23d308dc8224d56d345d46e
Werberth <werberthvinicius@gmail.com>
2020-01-11 04:27:33 -0300

    fix: change class_id for course foreignkey on activity model

--- class_path_web/content/forms.py
+++ class_path_web/content/forms.py
@@ -15,7 +15,7 @@ class ActivityForm(ModelForm):
         model = models.Activity
         fields = (
             'title', 'description', 'location',
-            'content', 'class_id', 'multimedia_required'
+            'content', 'course', 'multimedia_required'
         )
 
     def __init__(self, *args, **kwargs):
@@ -23,15 +23,13 @@ class ActivityForm(ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['location'].queryset = self.get_field_queryset('locations')
         self.fields['content'].queryset = self.get_field_queryset('contents')
-        self.fields['class_id'].queryset = self.get_classes()
+        self.fields['course'].queryset = self.get_classes()
 
     def get_field_queryset(self, attr):
         teacher = self.request.user.teacher
         queryset = getattr(teacher, attr).all()
         return queryset
 
-    def get_classes(self):
+    def get_course(self):
         teacher = self.request.user.teacher
-        ids = teacher.courses.values_list('class_id__id')
-        queryset = models.Class.objects.filter(id__in=ids)
-        return queryset
+        return teacher.courses.all()
